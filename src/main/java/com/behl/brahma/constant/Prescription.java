f3a21c8648427e55ea5ed630a2cf69583023cdc4
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-04 17:16:24 +0530

    added constants

---
+++ src/main/java/com/behl/brahma/constant/Prescription.java
@@ -0,0 +1,9 @@
+package com.behl.brahma.constant;
+
+public enum Prescription {
+
+    BUDWEISER, KINGFISHER, 
+    BLENDERS_PRIDE, ROYAL_STAG,
+    PATANALI_AAMRAS, PATANJALI_GILOY_AMLA;
+
+}
---
+++ src/main/java/com/behl/brahma/constant/Symptom.java
@@ -0,0 +1,7 @@
+package com.behl.brahma.constant;
+
+public enum Symptom {
+
+    FEVER, COUGH, HEADACHE, EYE_ACHE, COLD;
+
+}
