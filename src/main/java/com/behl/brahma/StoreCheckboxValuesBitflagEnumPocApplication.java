7b8033d893e95621cfa4994857520eacf14e0e8c
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-10-04 16:49:18 +0530

    init project commit

---
+++ src/main/java/com/behl/brahma/StoreCheckboxValuesBitflagEnumPocApplication.java
@@ -0,0 +1,13 @@
+package com.behl.brahma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCheckboxValuesBitflagEnumPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreCheckboxValuesBitflagEnumPocApplication.class, args);
+	}
+
+}
